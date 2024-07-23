f3ad3030f416040a2a583e6b9009e077cd665687
=SantiagoBarrientosF <barrientosantiago1090@gmail.com>
2024-07-23 14:39:07 -0500

    Fixes v 2

--- Area_comercial/api/contar.py
+++ Area_comercial/api/contar.py
@@ -1,50 +1,53 @@
 from Area_comercial.api.serializer import *
 from Area_comercial.models import Informe
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status 
 from collections import Counter
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from datetime import date
+from .serializer import InformeSerializer
 from django.http import JsonResponse
+from collections import defaultdict
 
 class Contarestados(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     def get(self,request):
-        
-        Meses = {
-            'Enero' : 1,
-            'Febrero' : 2,
-            'Marzo' : 3,
-            'Abril' : 4,
-            'Mayo' : 5,
-            'Junio' : 6,
-            'Julio' : 7,
-            'Agosto' : 8,
-            'Septiembre' : 9,
-            'Octubre' : 10,
-            'Noviembre' : 11,
-            'Diciembre' : 12,
-            
-        }
-        # Today = date.today()
-        # Mes_actual = Today.month
-        # print(Mes_actual)
-        # print (Meses)
-        # if Mes_actual in Meses:
         estados_contar = {
-            'Analisis por el cliente' :'Analisis por el cliente',
-            'Cerrado' : 'Cerrado',
-            'Negado' : 'Negado',
-         }
-            
+           'Analisis por el cliente' :'Analisis por el cliente',
+           'Cerrado' : 'Cerrado',
+           'Negado' : 'Negado',
+        }
+           
         informes = Informe.objects.all()
-
         estado_contado = Counter(Informe.Estado for Informe in informes if Informe.Estado in estados_contar)
-                
+               
         return JsonResponse({'estados_contador': dict(estado_contado)}, status=status.HTTP_200_OK)   
-        # else:
-        #     return JsonResponse({'message':'No se pueden mostrar los datos'}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+    
+class ContarestadoMes(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        estados_contar = {
+           'Analisis por el cliente' :'Analisis por el cliente',
+           'Cerrado' : 'Cerrado',
+           'Negado' : 'Negado',
+        }
+        informes = Informe.objects.all()
+        estados_por_mes = defaultdict(Counter)
+
+        for informe in informes:
+            if informe.Estado in estados_contar:
+                estados_por_mes[informe.Mes][informe.Estado] += 1
+                
      
+        estados_por_mes = {mes: dict(contador) for mes, contador in estados_por_mes.items()}
+                
+        return JsonResponse({'estados_por_mes': estados_por_mes}, status=status.HTTP_200_OK)
+
+        
+        
+        
+   
--- Area_comercial/api/urls.py
+++ Area_comercial/api/urls.py
@@ -4,17 +4,18 @@ from . import views
 from . import getdata
 from django.contrib.auth import views as auth_views
 from Area_comercial.api.cargararchivo import Cargararchivo
-from Area_comercial.api.contar import Contarestados
+from Area_comercial.api.contar import Contarestados, ContarestadoMes
 from Area_comercial.api.getdata import Save,Update
 urlpatterns = [
     path('login/', views.login ),
     path('register/', views.register ),
     path('contact/', Save.as_view()),
-    path('update/<int:id>', Update.as_view()),
+    path('contact/<int:id>', Update.as_view()),
     # path('update_estado/<int:id>', getdata.Hablitar),
     # path('get_informe/', getdata.informe_list),
     path('cargar-archivos/', Cargararchivo),
     path('contar/', Contarestados.as_view()),
+    path('contar_mes/', ContarestadoMes.as_view()),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
